1bdfb403c516ca4dcce66485ae964d9f7c9cafa6
hojinchang <hojinchang1998@gmail.com>
2023-09-08 18:12:21 -0700

    Add operate function

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(a, b, operator) {
+    let result;
+    switch (operator) {
+        case "add":
+            result = add(a, b);
+            break;
+        case "subtract":
+            result = subtract(a, b);
+            break;
+        case "multiply":
+            result = multiply(a, b);
+            break;
+        case "divide":
+            result = divide(a, b);
+            break;
+    }
+
+    return result;
+}
